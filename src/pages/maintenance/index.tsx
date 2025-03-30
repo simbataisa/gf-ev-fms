30e2fc9aa2497d8c91d6a63e68eed34650c0592a
Dennis Dao <v.cto@fgf.vn>
2025-03-30 22:23:20 +0700

    refactor(maintenance): comment out unused parser function
    The parser function in the MaintenancePage component was commented out as it is currently unused and to avoid potential issues with undefined values. This change improves code clarity and maintainability.

--- src/pages/maintenance/index.tsx
+++ src/pages/maintenance/index.tsx
@@ -301,7 +301,7 @@ const MaintenancePage: React.FC = () => {
               precision={2} 
               style={{ width: '100%' }} 
               formatter={value => `$ ${value}`.replace(/\B(?=(\d{3})+(?!\d))/g, ',')}
-              parser={value => value!.replace(/\$\s?|(,*)/g, '')}
+            //   parser={value => value ? parseFloat(value.replace(/\$\s?|(,*)/g, '')) : 0}
             />
           </Form.Item>
           
